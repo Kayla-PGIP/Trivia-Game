2373f69089bcd906cd4a0ec3edd8fcb398d57a15
Kayla-PGIP <168926178+Kayla-PGIP@users.noreply.github.com>
2024-06-28 19:17:40 -0400

    Add files via upload

---
+++ Form3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trivia_game_4
+{
+    public partial class Form3 : Form
+    {
+        public static string fk_ad;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string user = textBox1.Text;
+            string password = textBox2.Text;
+            string userdb, passworddb;
+
+            ReturnClass rc = new ReturnClass();
+            userdb = rc.scalarReturn("select count(ad_id) from admin_athu where ad_user=" + user + " ' ");
+
+            if (userdb != null)
+            {
+                MessageBox.Show("Invalid user name!");
+            }
+            else
+            {
+                passworddb = rc.scalarReturn("select ad_password from admin_athu where ad_user=" + user + " ' ");
+
+                if (passworddb.Equals(password))
+                {
+                    fk_ad = rc.scalarReturn("select ad_id from admin_athu where ad_user='" + user + " ' ");
+
+                    Form2 f = new Form2();
+                    f.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Password!");
+                }
+            }
+    }
+}
---
+++ ReturnClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trivia_game_4
+{
+    internal class ReturnClass
+    {
+    
+      private string connstring = ConfigurationManager.ConnectionString["test"].ConnectionString;
+
+
+        public string scalarReturn(string q)
+        {
+            string s;
+            SqlConnection conn = new SqlConnection(connstring); //connection obj
+            conn.Open(); //opening connection
+            try
+            {
+                SqlCommand cmd = new SqlCommand(q, conn);  //query execution
+
+                s = cmd.ExecuteScalar().ToString;
+            }
+            catch (Exception)
+            {
+                s = "";
+            }
+            conn.Close();
+
+            return s;
+        }
+
+    }
+}
